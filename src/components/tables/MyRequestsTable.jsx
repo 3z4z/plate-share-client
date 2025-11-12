b57406ba6aefe055de5c13cf284f5a070b5457c0
Md. Salman Ezaz <md.salmanezaz@gmail.com>
2025-11-12 12:02:20 +0600

    Added spinner and loaders for my requests table

--- src/components/tables/MyRequestsTable.jsx
+++ src/components/tables/MyRequestsTable.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router";
-import CommonTitleComponent from "../common/CommonTitle";
 import { useFoodsStore } from "../../stores/useFoodsStore";
 import { useEffect, useState } from "react";
 import RequestDeleteConfirmModal from "../modals/RequestDeleteConfirmModal";
 import cookingIcon from "../../assets/cooking.png";
+import SpinnerLoader from "../loaders/SpinnerLoader";
+import FieldSkeletonLoader from "../loaders/FieldSkeletonLoader";
 
 export default function MyRequestsTable({ myRequests }) {
   const { foods, setFoods, isFoodsLoading } = useFoodsStore();
@@ -13,122 +14,127 @@ export default function MyRequestsTable({ myRequests }) {
     setFoods();
   }, [setFoods]);
   return (
-    <div className="px-3 max-w-[1600px] mx-auto ">
-      <CommonTitleComponent title={"My Requested Plates"} margins={"my-7"} />
-      <h5 className="text-xl mb-5">
-        <span>Your Total Requests:</span>
-        <span className="text-primary ms-1">{myRequests.length}</span>
-      </h5>
-      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-white">
-        <table className="table">
-          <thead className="bg-base-200">
-            <tr>
-              <th className="w-1/16">Sl No.</th>
-              <th className="w-4/16">Requested Foods</th>
-              <th className="w-3/16">Plate Owner</th>
-              <th className="w-2/16 text-center">Quantity</th>
-              <th className="w-2/16 text-center">Request Status</th>
-              <th className="w-3/16 text-center">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {myRequests.length > 0 ? (
-              myRequests.map((request, index) => {
-                const { requestStatus, foodId } = request;
-                const requestedFood = foods.find((food) => food._id === foodId);
-                return (
-                  <tr key={index}>
-                    <td className="font-bold ps-5">{index + 1}</td>
-                    <td>
-                      <div className="flex gap-3 items-center">
-                        <div>
-                          <figure className="w-10 h-10 rounded-sm flex items-center justify-center border border-base-300 bg-gray-300 overflow-hidden">
-                            <img
-                              src={
-                                isFoodsLoading
-                                  ? "https://picsum.photos/200/300.jpg"
-                                  : requestedFood?.image || cookingIcon
-                              }
-                              alt=""
-                              className="w-full h-full object-cover"
-                            />
-                          </figure>
-                        </div>
-                        <span>
-                          {isFoodsLoading
-                            ? "Loading..."
-                            : requestedFood?.name || "Unknown"}
-                        </span>
-                      </div>
-                    </td>
-                    <td>
+    <table className="table">
+      <thead className="bg-base-200">
+        <tr>
+          <th className="w-1/16">Sl No.</th>
+          <th className="w-4/16">Requested Foods</th>
+          <th className="w-3/16">Plate Owner</th>
+          <th className="w-2/16 text-center">Quantity</th>
+          <th className="w-2/16 text-center">Request Status</th>
+          <th className="w-3/16 text-center">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {myRequests.length > 0 ? (
+          myRequests.map((request, index) => {
+            const { requestStatus, foodId } = request;
+            const requestedFood = foods.find((food) => food._id === foodId);
+            return (
+              <tr key={index} className="odd:bg-base-100 *:py-1.5">
+                <td className="font-bold ps-5">{index + 1}</td>
+                <td>
+                  <div className="flex gap-3 items-center">
+                    <div>
+                      {isFoodsLoading ? (
+                        <SpinnerLoader size={"loading-xl"} />
+                      ) : (
+                        <figure className="w-10 h-10 rounded-sm flex items-center justify-center border border-base-300 bg-gray-300 overflow-hidden">
+                          <img
+                            src={requestedFood?.image || cookingIcon}
+                            alt={`${requestedFood.name}'s image`}
+                            className="w-full h-full object-cover"
+                          />
+                        </figure>
+                      )}
+                    </div>
+                    {isFoodsLoading ? (
+                      <FieldSkeletonLoader />
+                    ) : (
+                      <span>{requestedFood?.name || "Unknown"}</span>
+                    )}
+                  </div>
+                </td>
+                <td>
+                  {isFoodsLoading ? (
+                    <FieldSkeletonLoader />
+                  ) : (
+                    <p>{requestedFood?.donor_name || "Unknown"}</p>
+                  )}
+                  <div className={`text-xs ${isFoodsLoading ? "mt-1" : ""}`}>
+                    {isFoodsLoading ? (
+                      <FieldSkeletonLoader />
+                    ) : (
                       <p>
-                        {isFoodsLoading
-                          ? "Loading..."
-                          : requestedFood?.donor_name || "Unknown"}
-                      </p>
-                      <p className="text-xs">
                         <span className="me-1">Pickup:</span>
-                        {isFoodsLoading
-                          ? "Loading..."
-                          : requestedFood?.pickup_location || "Unknown"}
+                        <span>
+                          {requestedFood?.pickup_location || "Unknown"}
+                        </span>
                       </p>
-                    </td>
-                    <td className="text-center">
-                      {isFoodsLoading
-                        ? "Loading..."
-                        : requestedFood?.quantity || "Unknown"}
-                    </td>
-                    <td className="text-center">
-                      <div
-                        className={`badge badge-sm ${
-                          requestStatus === "Pending"
-                            ? "badge-warning"
-                            : requestStatus === "Accepted"
-                            ? "badge-success"
-                            : "badge-error"
-                        }`}
+                    )}
+                  </div>
+                </td>
+                <td className="text-center">
+                  {isFoodsLoading ? (
+                    <FieldSkeletonLoader />
+                  ) : (
+                    <p>{requestedFood?.quantity || "Unknown"}</p>
+                  )}
+                </td>
+                <td className="text-center">
+                  {isFoodsLoading ? (
+                    <FieldSkeletonLoader />
+                  ) : (
+                    <div
+                      className={`badge badge-sm ${
+                        requestStatus === "Pending"
+                          ? "badge-warning"
+                          : requestStatus === "Accepted"
+                          ? "badge-success"
+                          : "badge-error"
+                      }`}
+                    >
+                      {requestStatus}
+                    </div>
+                  )}
+                </td>
+                <td>
+                  {isFoodsLoading ? (
+                    <FieldSkeletonLoader />
+                  ) : (
+                    <div className="flex gap-3 justify-center items-center">
+                      <Link
+                        to={`/food/${foodId}`}
+                        className="btn btn-primary btn-sm"
                       >
-                        {requestStatus}
-                      </div>
-                    </td>
-                    <td>
-                      <div className="flex gap-3 justify-center items-center">
-                        <Link
-                          to={`/food/${foodId}`}
-                          className="btn btn-primary btn-sm"
-                        >
-                          View
-                        </Link>
-                        <button
-                          onClick={() => setIsDeleteModalOpen(true)}
-                          className="btn btn-error btn-soft btn-sm"
-                        >
-                          Delete
-                        </button>
-                        <RequestDeleteConfirmModal
-                          isDeleteModalOpen={isDeleteModalOpen}
-                          setIsDeleteModalOpen={setIsDeleteModalOpen}
-                          foodId={foodId}
-                          request={request}
-                        />
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })
-            ) : (
-              <tr>
-                <td colSpan={6} className="py-10 text-center ">
-                  <p className="text-xl text-error mb-6">
-                    No Requests in the list
-                  </p>
+                        View
+                      </Link>
+                      <button
+                        onClick={() => setIsDeleteModalOpen(true)}
+                        className="btn btn-error btn-soft btn-sm"
+                      >
+                        Delete
+                      </button>
+                      <RequestDeleteConfirmModal
+                        isDeleteModalOpen={isDeleteModalOpen}
+                        setIsDeleteModalOpen={setIsDeleteModalOpen}
+                        foodId={foodId}
+                        request={request}
+                      />
+                    </div>
+                  )}
                 </td>
               </tr>
-            )}
-          </tbody>
-        </table>
-      </div>
-    </div>
+            );
+          })
+        ) : (
+          <tr>
+            <td colSpan={6} className="py-10 text-center ">
+              <p className="text-xl text-error mb-6">No Requests in the list</p>
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
   );
 }
--- src/pages/private/MyRequests.jsx
+++ src/pages/private/MyRequests.jsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import { useRequestStore } from "../../stores/useRequestStore";
 import { useAuthStore } from "../../stores/useAuthStore";
 import MyRequestsTable from "../../components/tables/MyRequestsTable";
+import CommonTitleComponent from "../../components/common/CommonTitle";
+import SpinnerLoader from "../../components/loaders/SpinnerLoader";
 
 export default function MyRequestsPage() {
   const { user } = useAuthStore();
@@ -13,11 +15,26 @@ export default function MyRequestsPage() {
   }, [setMyRequests, user]);
   return (
     <>
-      {isRequestsLoading ? (
+      <div className="px-3 max-w-[1600px] mx-auto ">
+        <CommonTitleComponent title={"My Requested Plates"} margins={"my-7"} />
+        <h5 className="text-xl mb-5">
+          <span>Your Total Requests:</span>
+          <span className="text-primary ms-1">
+            {isRequestsLoading ? (
+              <SpinnerLoader size={"loading-xl"} />
+            ) : (
+              myRequests.length
+            )}
+          </span>
+        </h5>
+        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-white">
+          <MyRequestsTable myRequests={myRequests} />
+        </div>
+      </div>
+      {/* {isRequestsLoading ? (
         <p>Loading...</p>
       ) : (
-        <MyRequestsTable myRequests={myRequests} />
-      )}
+      )} */}
     </>
   );
 }
